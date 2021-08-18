fd1c59ed653d2169358876f500c322e1f1425b27
Sander Boogaards <sanderboogaards@gmail.com>
2021-08-18 15:13:30 +0200

    Solution Rails Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+Restaurant.create(name: "Napoli", address: "Napels", category: "italian")
+Restaurant.create(name: "Sushi", address: "Tokio", category: "japanese")
+Restaurant.create(name: "Mie", address: "Beijing", category: "chinese")
+Restaurant.create(name: "Entrecote", address: "Paris", category: "french")
+Restaurant.create(name: "Fries", address: "Antwerp", category: "belgian")
+
+puts "Finished!"
